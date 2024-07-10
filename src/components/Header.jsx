973cf074bbf9915ae9534ada9c2168e2d9c0199d
Yorman <yjona@espe.edu.ec>
2024-07-10 12:37:56 -0500

    V2.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+import Header from './Header';
+
+const MainContent = () => (
+    <div>
+        <Header />
+        <div className="p-4">
+            <h1>Welcome to the Main Content</h1>
+            <p>
+            This is the main content area. You can add any content here. You can
+            also add more components to this area.
+            </p>
+        </div>
+    </div>
+);
+export default MainContent;
